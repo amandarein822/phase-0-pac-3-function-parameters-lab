d28cd455f9d92fb5c3d1f53900a5a41471965724
Amanda <wiltbergeramanda@gmail.com>
2023-11-17 22:34:40 -0600

    complete

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+    console.log("Aki")
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    console.log("Aki")
+    console.log("German")
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    console.log("Aki")
+}
+
+
+
